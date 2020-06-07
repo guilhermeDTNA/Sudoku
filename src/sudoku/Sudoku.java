f369b2c66d27fc5ce4bcf0924919af00924f3daa
guilherme <gleite98@gmail.com>
2020-06-07 14:14:45 -0300

    Aplicando novo método

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -24,20 +24,39 @@ public class Sudoku {
     protected static int linha;
     protected static int coluna;
 
+    //Após modificações
+    private static int numeros[] = new int[9]; //contém todos os valores de 1 a 9
+    private static int number; //valor da posição atual do vetor
+    private static int contadorPosicao = 0; //verifica quantos números já foram gerados por posição da matriz
+    private int contadorSequencia = 0;
+
+    private static List<Integer> numerosGerados = new ArrayList<>(); //armazena quais números já foram gerados
+    private static List<Integer> numerosAusentes = new ArrayList<>(); //armazena quais números estão faltando na linha
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        // TODO code application logic here
+
         LeituraArquivo.lerArq();
         programa.armazenaValoresQuadros();
-        programa.insereNumeros();
+        geraNumerosEstaticos();
 
-        while (programa.analisaMatriz() == false) {
+        do {
+
             programa.insereNumeros();
-        }
+
+            programa.contadorSequencia++;
+        } while (programa.analisaMatriz() == false);
+
         programa.exibirMatriz();
 
+    }
+
+    public static void geraNumerosEstaticos() {
+        for (int i = 0; i < LeituraArquivo.dimensoes; i++) {
+            numeros[i] = i + 1;
+        }
     }
 
     public void armazenaValoresQuadros() {
@@ -95,21 +114,6 @@ public class Sudoku {
                 quadros.add(quadro);
 
             }
-            /*
-            int indiceQuadrante = contadorQuadro + 1;
-            while (contadorQuadro < 9) {
-                System.out.println(indiceQuadrante + "º quadrante:");
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        System.out.print(quadros.get(contadorQuadro).getNumeros(contadorValor));
-                        contadorValor++;
-                    }
-                    System.out.println("");
-                }
-                contadorValor = 0;
-                contadorQuadro++;
-                indiceQuadrante++;
-            }*/
 
         } else if (LeituraArquivo.dimensoes == 15) {
             //quadros = new Quadro(LeituraArquivo.matriz[0][0], LeituraArquivo.matriz[0][1], LeituraArquivo.matriz[0][2], LeituraArquivo.matriz[0][3], LeituraArquivo.matriz[0][4], LeituraArquivo.matriz[1][0], LeituraArquivo.matriz[1][1], LeituraArquivo.matriz[1][2], LeituraArquivo.matriz[1][3], LeituraArquivo.matriz[1][4], LeituraArquivo.matriz[2][0], LeituraArquivo.matriz[2][1], LeituraArquivo.matriz[2][2], LeituraArquivo.matriz[2][3], LeituraArquivo.matriz[2][4]);
@@ -121,87 +125,241 @@ public class Sudoku {
         }
     }
 
-    public boolean insereNumeros() {
-
-        int numero = geraNumero();
-        int contador = 0;
+    public void atribuiValores() {
+        List<Integer> numerosPresentes = new ArrayList<>(); //armazena quais números estão faltando na linha
 
         for (int i = 0; i < LeituraArquivo.dimensoes; i++) {
             for (int j = 0; j < LeituraArquivo.dimensoes; j++) {
-                if ((verificaDisponibilidadeValoresFixos(i, j) == false)) { //Já existe um valor fixo na linha e coluna
-                    
-                } else if (verificaDisponibilidade(numero, i, j) == false) { //Já existe o valor na linha ou coluna
-                    do {
-                        numero = geraNumero();
-                        
-                        contador++;
-                    } while ((contador <= LeituraArquivo.dimensoes) && (verificaDisponibilidade(numero, i, j) == false));
-                    if (contador > LeituraArquivo.dimensoes) {
-                        substituiNumeros(numero);
-                    }
+                if (LeituraArquivo.matriz[i][j] == 0) {
+                    numerosPresentes = insereValoresPresentes(i, numerosPresentes);
+                    insereValoresAusentes(i, j, numerosPresentes);
+                    atribuiValores(i, j, LeituraArquivo.matriz[i][j]);
+                }
+            }
+        }
+    }
 
-                } else if (analisaQuadrantes(numero, i, j) == false) {
-                    contador = 0;
-                    do {
-                        numero = geraNumero();
-                        /*
-                        if (j == 0) {
-                            i--;
-                        } else {
-                            j--;
-                        }
-*/
-                        contador++;
-                    } while ((contador <= LeituraArquivo.dimensoes) && (analisaQuadrantes(numero, i, j) == false));
-                    if (contador > LeituraArquivo.dimensoes) {
-                        substituiNumeros(numero);
-                    }
+    public List<Integer> insereValoresPresentes(int linha, List<Integer> numeros) {
+        for (int j = 0; j < LeituraArquivo.dimensoes; j++) {
+            if (LeituraArquivo.matriz[linha][j] != 0) {
+                numeros.add(LeituraArquivo.matriz[linha][j]);
+            }
+        }
 
+        return numeros;
+    }
+
+    public void insereValoresAusentes(int linha, int coluna, List<Integer> numeros) {
+        int contadorNumerosDiferentes = 0;
+
+        for (int i = 0; i < this.numeros.length; i++) {
+            for (int valor : numeros) {
+                if (this.numeros[i] != numeros.get(i)) {
+                    contadorNumerosDiferentes++;
+                    if (contadorNumerosDiferentes == numeros.size()) {
+                        numerosAusentes.add(numeros.get(i));
+                    } else {
+                        valor++;
+                    }
+                }
+            }
+        }
+    }
+
+    public void atribuiValores(int linha, int coluna, int valor) {
+        //Atribui os um número ausente na célula e verifica onde ele já está, setando-o pra zero
+        int posicao[] = new int[2];
+
+        if (verificaDisponibilidadeValoresFixos(linha, coluna) == false) {
+            return;
+        } else {
+
+            if (verificaDisponibilidadeValoresFixos(linha, coluna) == false) {
+                return;
+            } else {
+                posicao[0] = retornaPosicoesLinha(valor, linha)[0];
+                posicao[1] = retornaPosicoesLinha(valor, linha)[1];
+
+                if (posicao[0] != -1) {
+                    LeituraArquivo.matriz[posicao[0]][posicao[1]] = 0;
+
+                    return;
                 } else {
-                    LeituraArquivo.matriz[i][j] = numero; //Insere o número na matriz
+                    posicao[0] = retornaPosicoesColuna(valor, coluna)[0];
+                    posicao[1] = retornaPosicoesColuna(valor, coluna)[1];
+
+                    if (posicao[0] != -1) {
+                        LeituraArquivo.matriz[posicao[0]][posicao[1]] = 0;
+                        return;
+                    }
+                }
+
+                int quadrante = retornaQuadrante(linha, coluna);
+
+                if (verificaDisponibilidade(valor, quadrante) == false) {
+                    LeituraArquivo.matriz[linha][coluna] = 0;
+                    return;
+                }
+
+            }
+
+        }
+
+        //if(retornaPosicoesLinha(number, linha))
+        retornaPosicoesColuna(number, coluna);
+    }
+
+    public boolean insereNumerosPosicaoDeterminada(int numero, int linha, int coluna) {
+
+        //int numero = geraNumero();
+        if (numero == 0) {
+            numero = geraNumero();
+        }
+
+        if (coluna < LeituraArquivo.dimensoes) {
+            for (int i = 0; i < LeituraArquivo.dimensoes; i++) {
+                for (int j = coluna; j < LeituraArquivo.dimensoes; j++) {
+                    if (verificaZeros(i, j)) {
+                        j = LeituraArquivo.dimensoes;
+                    } else if ((verificaDisponibilidadeValoresFixos(i, j) == false)) { //Já existe um valor fixo na linha e coluna
+                        j = LeituraArquivo.dimensoes;
+                    } else if (verificaLinha(numero, i) == false) { //Já existe o valor na linha
+                        //Sudoku.contadorPosicao++;
+                        Sudoku.numerosGerados.add(numero);
+                        verificaNumeros(numero, i, j);
+                    } else if (verificaColuna(numero, j) == false) { //Já existe o valor na coluna
+                        //Sudoku.contadorPosicao++;
+                        Sudoku.numerosGerados.add(numero);
+                        verificaNumeros(numero, i, j);
+                    } else if (analisaQuadrantes(numero, i, j) == false) { //Já existe o valor no quadrante
+                        //Sudoku.contadorPosicao++;
+                        Sudoku.numerosGerados.add(numero);
+                        verificaNumeros(numero, i, j);
+                    } else {
+                        LeituraArquivo.matriz[i][j] = numero;
+                        Sudoku.contadorPosicao = 0;
+                        limparNumerosGerados();
+                        j = LeituraArquivo.dimensoes;
+                    }
+
+                }
+            }
+        } else {
+            for (int i = linha + 1; i < LeituraArquivo.dimensoes; i++) {
+                for (int j = 0; j < LeituraArquivo.dimensoes; j++) {
+                    if (verificaZeros(i, j)) {
+                        j = LeituraArquivo.dimensoes;
+                    } else if ((verificaDisponibilidadeValoresFixos(i, j) == false)) { //Já existe um valor fixo na linha e coluna
+                        j = LeituraArquivo.dimensoes;
+                    } else if (verificaLinha(numero, i) == false) { //Já existe o valor na linha
+                        //Sudoku.contadorPosicao++;
+                        Sudoku.numerosGerados.add(numero);
+                        verificaNumeros(numero, i, j);
+                    } else if (verificaColuna(numero, j) == false) { //Já existe o valor na coluna
+                        //Sudoku.contadorPosicao++;
+                        Sudoku.numerosGerados.add(numero);
+                        verificaNumeros(numero, i, j);
+                    } else if (analisaQuadrantes(numero, i, j) == false) { //Já existe o valor no quadrante
+                        //Sudoku.contadorPosicao++;
+                        Sudoku.numerosGerados.add(numero);
+                        verificaNumeros(numero, i, j);
+                    } else {
+                        LeituraArquivo.matriz[i][j] = numero;
+                        Sudoku.contadorPosicao = 0;
+                        limparNumerosGerados();
+                        j = LeituraArquivo.dimensoes;
+                    }
+
                 }
             }
         }
         return true;
     }
 
-    public void substituiNumeros(int valor) {
+    public boolean insereNumeros() {
 
         int numero = geraNumero();
-        int contador = 0;
 
-        if (verificaDisponibilidade(numero, Sudoku.linha, Sudoku.coluna) == false) { //Já existe o valor na linha ou coluna
-            do {
-                numero = geraNumero();
-                contador++;
-
-                if (contador >= LeituraArquivo.dimensoes) { //Se já tentou todas as opções, retorna à matriz original
-                    insereNumeros();
+        for (int i = 0; i < LeituraArquivo.dimensoes; i++) {
+            for (int j = 0; j < LeituraArquivo.dimensoes; j++) {
+                if (verificaZeros(i, j)) {
+                    j = LeituraArquivo.dimensoes;
+                } else if ((verificaDisponibilidadeValoresFixos(i, j) == false)) { //Já existe um valor fixo na linha e coluna
+                    j = LeituraArquivo.dimensoes;
+                } else if (verificaLinha(numero, i) == false) { //Já existe o valor na linha
+                    //Sudoku.contadorPosicao++;
+                    Sudoku.numerosGerados.add(numero);
+                    verificaNumeros(numero, i, j);
+                } else if (verificaColuna(numero, j) == false) { //Já existe o valor na coluna
+                    //Sudoku.contadorPosicao++;
+                    Sudoku.numerosGerados.add(numero);
+                    verificaNumeros(numero, i, j);
+                } else if (analisaQuadrantes(numero, i, j) == false) { //Já existe o valor no quadrante
+                    //Sudoku.contadorPosicao++;
+                    Sudoku.numerosGerados.add(numero);
+                    verificaNumeros(numero, i, j);
+                } else {
+                    LeituraArquivo.matriz[i][j] = numero;
+                    Sudoku.contadorPosicao = 0;
+                    limparNumerosGerados();
+                    j = LeituraArquivo.dimensoes;
                 }
-            } while (verificaDisponibilidade(numero, linha, coluna) == false);
 
-        } if (analisaQuadrantes(numero, linha, coluna) == false) {
-            contador = 0;
-            do {
+            }
+        }
+        return true;
+    }
+
+    public boolean verificaZeros(int linha, int coluna) {
+        if (LeituraArquivo.matriz[linha][coluna] == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public void verificaNumeros(int numero, int linhaAtual, int colunaAtual) {
+        if (Sudoku.contadorPosicao <= 9) {
+            //int gerador = geraNumero();
+            if (numero == 0) {
+                Sudoku.contadorPosicao = 0;
+                limparNumerosGerados();
                 numero = geraNumero();
-                contador++;
-
-                if (contador >= LeituraArquivo.dimensoes) { //Se já tentou todas as opções, retorna à matriz original
-                    insereNumeros();
-                }
-            } while (analisaQuadrantes(numero, linha, coluna) == false);
-
-        } if((verificaDisponibilidade(numero, linha, coluna) == true)&&(analisaQuadrantes(numero, linha, coluna) == true))
-            LeituraArquivo.matriz[linha][coluna] = numero; //Insere o número na matriz
-        
-
+                insereNumerosPosicaoDeterminada(numero, linhaAtual, colunaAtual + 1);
+            } else if (numero == -1) {
+                numero = geraNumero();
+                Sudoku.contadorPosicao++;
+                verificaNumeros(numero, linhaAtual, colunaAtual);
+            }
+        } else {
+            Sudoku.contadorPosicao = 0;
+            limparNumerosGerados();
+            numero = geraNumero();
+            insereNumerosPosicaoDeterminada(numero, linhaAtual, colunaAtual + 1);
+        }
     }
 
     public int geraNumero() {
         int numero;
         numero = gerador.nextInt(LeituraArquivo.dimensoes) + 1; //Gera um número aleatório
 
+        if (Sudoku.numerosGerados.size() == 9) {
+            return 0; //já sorteou todos os números possíveis
+        } else {
+            for (int valor : Sudoku.numerosGerados) {
+                if (valor == numero) {
+                    return -1; //o número já foi sorteado
+                }
+            }
+        }
+        Sudoku.contadorPosicao++;
         return numero;
+
+    }
+
+    public void limparNumerosGerados() {
+        for (int valor : Sudoku.numerosGerados) {
+            Sudoku.numerosGerados.remove(valor);
+        }
     }
 
     public boolean verificaDisponibilidadeValoresFixos(int linha, int coluna) {
@@ -215,24 +373,56 @@ public class Sudoku {
         return true;
     }
 
-    public boolean verificaDisponibilidade(int numero, int linha, int coluna) { //Se o 
+    //Só verifica e retorna false=não pode inserir
+    public boolean verificaLinha(int numero, int linha) {
         for (int j = 0; j < LeituraArquivo.dimensoes; j++) {
             if (LeituraArquivo.matriz[linha][j] == numero) { //Verifica se ele já está na linha
-                Sudoku.linha = linha;
-                Sudoku.coluna = j;
+
                 return false;
             }
         }
+        return true;
+    }
+
+    public int[] retornaPosicoesLinha(int numero, int linha) {
+        int posicao[] = new int[2];
+
+        for (int j = 0; j < LeituraArquivo.dimensoes; j++) {
+            if (LeituraArquivo.matriz[linha][j] == numero) { //Verifica se ele já está na linha
+                posicao[0] = linha;
+                posicao[1] = j;
+                return posicao;
+            }
+        }
+        posicao[0] = -1;
+        posicao[1] = -1;
+        return posicao;
+    }
+
+    public boolean verificaColuna(int numero, int coluna) {
+        for (int i = 0; i < LeituraArquivo.dimensoes; i++) {
+            if (LeituraArquivo.matriz[i][coluna] == numero) { //Verifica se ele já está na linha
+                //Sudoku.linha = linha;
+                //Sudoku.coluna = j;
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int[] retornaPosicoesColuna(int numero, int coluna) {
+        int posicao[] = new int[2];
 
         for (int i = 0; i < LeituraArquivo.dimensoes; i++) {
-            if (LeituraArquivo.matriz[i][coluna] == numero) { //Verifica se ele já está na coluna
-                Sudoku.linha = i;
-                Sudoku.coluna = coluna;
-                return false;
+            if (LeituraArquivo.matriz[i][coluna] == numero) { //Verifica se ele já está na linha
+                posicao[0] = i;
+                posicao[1] = coluna;
+                return posicao;
             }
         }
-
-        return true;
+        posicao[0] = -1;
+        posicao[1] = -1;
+        return posicao;
     }
 
     public boolean analisaQuadrantes(int numero, int i, int j) {
@@ -287,7 +477,32 @@ public class Sudoku {
         return true;
     }
 
+    public int retornaQuadrante(int i, int j) {
+        if ((i >= 0 && i <= 2) && (j >= 0 && j <= 2)) { //primeiro quadrante
+            return 1;
+        } else if ((i >= 0 && i <= 2) && (j >= 3 && j <= 5)) { //segundo quadrante
+            return 2;
+        } else if ((i >= 0 && i <= 2) && (j >= 6 && j <= 8)) { //terceiro quadrante
+            return 3;
+        } else if ((i >= 3 && i <= 5) && (j >= 0 && j <= 2)) { //quarto quadrante
+            return 4;
+        } else if ((i >= 3 && i <= 5) && (j >= 3 && j <= 5)) { //quinto quadrante
+            return 5;
+        } else if ((i >= 3 && i <= 5) && (j >= 6 && j <= 8)) { //sexto quadrante
+            return 6;
+        } else if ((i >= 6 && i <= 8) && (j >= 0 && j <= 2)) { //sétimo quadrante
+            return 7;
+        } else if ((i >= 6 && i <= 8) && (j >= 3 && j <= 5)) { //oitavo quadrante
+            return 8;
+        } else if ((i >= 6 && i <= 8) && (j >= 6 && j <= 8)) { //nono quadrante
+            return 9;
+
+        }
+        return 0;
+    }
+
     public boolean analisaMatriz() {
+
         for (int i = 0; i < LeituraArquivo.dimensoes; i++) {
             for (int j = 0; j < LeituraArquivo.dimensoes; j++) {
                 if (LeituraArquivo.matriz[i][j] == 0) {
@@ -296,6 +511,8 @@ public class Sudoku {
                 }
             }
         }
+
+        //System.out.println(LeituraArquivo.dimensoes);
         return true;
     }
 
